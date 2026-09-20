602ffea12231363565dac91ff7c1b2130f230662
nihalchundru <nihalchundru@gmail.com>
2026-09-20 16:52:28 -0700

    Delete zen directory

--- zen/scripts/zen-css-watch.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-CSS="/home/nihal/.config/zen/o5lc5mru.Default (release)/chrome/userChrome.css"
-
-while inotifywait -e close_write "$CSS"; do
-    echo "Zen CSS updated"
-done
